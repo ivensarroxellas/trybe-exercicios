7ae1defe49827747451172d5c2393687a79ef00b
Ivens <ivensarroxellas@hotmail.com>
2022-05-31 18:04:15 -0300

    adiciona bloco5

---
+++ fundamentos/bloco-05-javascript-dom-eventos-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,4 @@
+let titulo = document.querySelector("#elementoOndeVoceEsta")
+titulo.parentNode.style.color = "red"
+let primeiroFilho = document.getElementById("primeiroFilhoDoFilho")
+primeiroFilho.innerText = "adiciona texto"
