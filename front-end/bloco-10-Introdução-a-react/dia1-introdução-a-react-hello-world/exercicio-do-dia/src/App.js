72a959ca1ff23b1cf0cd26fd3b7b39be2a4a8ab5
Ivens <ivensarroxellas@hotmail.com>
2022-07-18 20:02:23 -0300

    dia 10.1

---
+++ front-end/bloco-10-Introdução-a-react/dia1-introdução-a-react-hello-world/exercicio-do-dia/src/App.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import React, { Component } from 'react'
+import Content from './Content';
+
+export class App extends Component {
+  render() {
+    return (
+      <Header />
+      <Content />
+    )
+  }
+}
+
+export default App
